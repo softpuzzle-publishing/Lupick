c11f875be2303d76079c5a50c55ed923e4466e15
park-geun <rmstn133@gmail.com>
2020-12-31 13:01:59 +0900

    이벤트 상세, 랜덤투표 모달

--- webview/resources/js/common.js
+++ webview/resources/js/common.js
@@ -37,33 +37,21 @@ $(document).ready(function () {
         });
         $(".select-md").each(function () {
             $(this).selectmenu().selectmenu("menuWidget").addClass("overflow select-md menuWidget");
-           
-            $('.ui-selectmenu-button').on('click', function () {
-                var option_height = $('.ui-menu').height();
-                var modal_height = $('.modal-content').height();
-                var total_height = option_height + modal_height;
-                var sel_length = $('.sel-option').length;
-                console.log(sel_length);
-                
-                if( sel_length  >= 1 ){
-                    $('.wrap').css('height', 'auto');
-                    return false;
+            var option_length = $('.ui-selectmenu-menu ul li').length;
+            $('.ui-selectmenu-button').click(function(){
+                setTimeout(function(){
+                    console.log(option_length);
+                },1000)
+            
+                if( $('[aria-expanded="true"]') ){
+                    if(option_length <= 3){
+                        $('.ui-selectmenu-menu .ui-menu').css('max-height','100px');
+                    }else if(option_legnth >= 4){
+                        $('.ui-selectmenu-menu .ui-menu').css('max-height','400px');
+                    }
                 }
-                if (!$('.ui-selectmenu-menu').hasClass('ui-selectmenu-open')) {
-                    $('.wrap').css('height', 'auto');
-    
-                } else {
-                    setTimeout(function () {
-                        $('.wrap').css('height', total_height);
-                    }, 100)
-                    setTimeout(function () {
-                        $(".select-md").selectmenu("open");
-                    }, 100)
-                }
-                $('.ui-selectmenu-menu').click(function () {
-                    $('.wrap').css('height', 'auto');
-                })
             })
+           
         });
         $(".select-lg").each(function () {
             $(this).selectmenu().selectmenu("menuWidget").addClass("overflow select-lg");
